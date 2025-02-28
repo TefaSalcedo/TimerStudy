c6509f3fd7ad05e00a07cf0ad19401d138b3224f
Tefa <stefa.21@hotmail.com>
2025-02-28 17:30:04 -0500

    inicio de proyecto, y reloj funcional

---
+++ index.js
@@ -0,0 +1,8 @@
+function updateClock() {
+    const now = new Date();
+    document.getElementById("hours").textContent = String(now.getHours()).padStart(2, '0');
+    document.getElementById("minutes").textContent = String(now.getMinutes()).padStart(2, '0');
+}
+
+setInterval(updateClock, 1000);
+updateClock();
